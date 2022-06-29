4491dcceab38621cfddfdd4a4e029abdcb0d92bb
ho byeong jin <77577434+jinhobyeong@users.noreply.github.com>
2022-06-29 14:32:14 +0900

    [Silver III] Title: N과 M (3), Time: 1516 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,28 @@
+import sys
+
+input = sys.stdin.readline
+
+n,m = list(map(int,input().split()))
+ 
+s = []
+
+
+ 
+def dfs(start):
+    if len(s)==m:
+        print(' '.join(map(str,s)))
+        return
+
+
+    for i in range(1,n+1):
+        #print(i)
+        s.append(i)
+        #print(s)
+        #print("dfs 실행")
+        dfs(i)
+        #print("dfs 종료")
+        s.pop()
+        #print(s)
+
+dfs(1)
+
