41db9e00b6f2ed4f0e3add293f4e7d21e77a97f9
ho byeong jin <77577434+jinhobyeong@users.noreply.github.com>
2022-07-24 21:28:17 +0900

    [Gold V] Title: 토마토, Time: 3220 ms, Memory: 113244 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -20,33 +20,30 @@ for i in range(N):
       q.append([i,j]) # 아예 배열 할당하면서 익은 토마토 위치 큐에 추가
       visited[i][j] == 1 # 방문한걸로 치자 
 
-# 처음에 아예 다 익었다면 그냥 0 출력
-test = True
+
+while q : 
+  x,y = q.popleft()
+
+  for i in range(4) : 
+    X = x + ox[i] 
+    Y = y + oy[i]
+
+    if (0 <= X < N) and (0 <= Y < M) and visited[X][Y] == 0 and box[X][Y] == 0: # 방문을 안했고, 익지 않은 토마토고 배열에 안벗어난다면 
+      visited[X][Y] = 1 # 방문한 걸로 만들고 
+      box[X][Y] = box[x][y] + 1 # box에 토마토가 익었다고 표시 밑 방문 일수 체크 ? 
+      q.append([X,Y])
+
+test = False
 for i in box:
     if 0 in i:
-      test = False
-if test :  # 이걸로 감싸야겠따. 
-  print(0)
+      test = True
+
+if test : 
+  print(-1)
 else : 
-  while q : 
-    x,y = q.popleft()
+  maxValue = max(map(max, box))
+  print(maxValue-1)
 
-    for i in range(4) : 
-      X = x + ox[i] 
-      Y = y + oy[i]
 
-      if (0 <= X < N) and (0 <= Y < M) and visited[X][Y] == 0 and box[X][Y] == 0: # 방문을 안했고, 익지 않은 토마토고 배열에 안벗어난다면 
-        visited[X][Y] = 1 # 방문한 걸로 만들고 
-        box[X][Y] = box[x][y] + 1 # box에 토마토가 익었다고 표시 밑 방문 일수 체크 ? 
-        q.append([X,Y])
 
-  test2 = False
-  for i in box:
-      if 0 in i:
-        test2 = True
 
-  if test2 : 
-    print(-1)
-  else : 
-    maxValue = max(map(max, box))
-    print(maxValue-1)
