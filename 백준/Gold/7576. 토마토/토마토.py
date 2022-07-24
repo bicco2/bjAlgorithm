dfe74c7bdefe4ee479afc9f7f6ffc0231483278d
ho byeong jin <77577434+jinhobyeong@users.noreply.github.com>
2022-07-24 21:30:15 +0900

    [Gold V] Title: 토마토, Time: 2968 ms, Memory: 105456 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -5,8 +5,6 @@ oy = [-1,1,0,0]
 
 M,N = map(int, input().split())
 
-#방문 확인 배열
-visited = [[0]* M for _ in range(N)]
 #토마토 상자 
 box = [[0] * M for _ in range(N)]
 
@@ -18,8 +16,6 @@ for i in range(N):
     box[i][j] = lineArr[j]
     if lineArr[j] == 1 : 
       q.append([i,j]) # 아예 배열 할당하면서 익은 토마토 위치 큐에 추가
-      visited[i][j] == 1 # 방문한걸로 치자 
-
 
 while q : 
   x,y = q.popleft()
@@ -28,8 +24,7 @@ while q :
     X = x + ox[i] 
     Y = y + oy[i]
 
-    if (0 <= X < N) and (0 <= Y < M) and visited[X][Y] == 0 and box[X][Y] == 0: # 방문을 안했고, 익지 않은 토마토고 배열에 안벗어난다면 
-      visited[X][Y] = 1 # 방문한 걸로 만들고 
+    if (0 <= X < N) and (0 <= Y < M) and box[X][Y] == 0: # 방문을 안했고, 익지 않은 토마토고 배열에 안벗어난다면 
       box[X][Y] = box[x][y] + 1 # box에 토마토가 익었다고 표시 밑 방문 일수 체크 ? 
       q.append([X,Y])
 
