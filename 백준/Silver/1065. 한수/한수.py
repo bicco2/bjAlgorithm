8ea062fe628ce0f873463b1f9483acb4b5247c34
bicco2 <77577434+bicco2@users.noreply.github.com>
2022-10-31 13:42:56 +0900

    [Silver IV] Title: 한수, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1065. 한수/한수.py
+++ 백준/Silver/1065. 한수/한수.py
@@ -1,12 +1,15 @@
-def hansu(num) :
-    hansu_cnt = 0
-    for i in range(1, num+1):
-        num_list = list(map(int,str(i)))
-        if i < 100:
-            hansu_cnt += 1  # 100보다 작으면 모두 한수
-        elif num_list[0]-num_list[1] == num_list[1]-num_list[2]:
-            hansu_cnt += 1  # x의 각 자리가 등차수열이면 한수
-    return hansu_cnt
+N = int(input())
 
-num = int(input())
-print(hansu(num))
+count = 0
+
+if N <= 99:
+    print(N)
+elif N == 1000 : 
+    print(144)
+else : 
+    for i in range(100 ,N+1) : 
+        num = list(str(i))
+        if(int(num[0]) - int(num[1]) == int(num[1]) - int(num[2])):
+            count += 1
+ 
+    print(99+ count)
