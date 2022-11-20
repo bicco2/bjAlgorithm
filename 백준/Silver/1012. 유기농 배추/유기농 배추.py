aed8c71c599d67c9a5fddb035216daf84f2cf602
bicco2 <77577434+bicco2@users.noreply.github.com>
2022-11-20 18:36:12 +0900

    [Silver II] Title: 유기농 배추, Time: 104 ms, Memory: 32436 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,44 +1,45 @@
+from collections import deque
 import sys
+input = sys.stdin.readline
 
-sys.setrecursionlimit(10000)
+def bfs(sx,sy) : 
+    ax = [0,0,-1,1]
+    ay = [-1,1,0,0]
+    queue = deque()
+
+    queue.append([sx,sy])
+
+    while queue:
+
+        x,y = queue.popleft()
+        
+        for i in range(4):
+            X = x + ax[i]
+            Y = y + ay[i]
+
+            # 근처 방문처가 범위 밖이 아니고 , 방문이 아직 안됐으며, 집이 있으면
+            if  (0<= X < N) and (0<= Y < M) and farm[X][Y] == 1 : 
+                queue.append([X,Y])
+                farm[X][Y] = 0
 
 
 
-def dfs(ox,oy) : 
-  cx = [1, -1, 0, 0]
-  cy = [0, 0, 1, -1]
-
-  for i in range(4):
-    X = ox + cx[i]
-    Y = oy + cy[i]
-
-    if (0 <= X < N) and (0 <= Y < M) : 
-      if graph[X][Y] == 1 : 
-        graph[X][Y] = 2 
-        dfs(X,Y)
-    
-
+           
 total = int(input())
-for _ in range(total) : 
-  M,N,K = map(int, input().split())
-  
-  cnt = 0
-  graph = [[0] * (M) for _ in range(N)]
-
-  for _ in range(K) : 
-    y,x = map(int, input().split())
-    graph[x][y] = 1 
-
-  for i in range(N):
-    for j in range(M): 
-      if graph[i][j] == 1 : 
-        dfs(i,j)
-        cnt += 1
-  print(cnt)
 
 
+for _ in range(total) :
+    M,N,K = map(int, input().split())
+    groupCnt = 0
+    farm = [[0 for _ in range(M)] for _ in range(N)]
 
+    for _ in range(K):
+        x,y = map(int, input().split())
+        farm[y][x] = 1 
 
-
-
-
+    for i in range(M): 
+        for j in range(N):
+            if farm[j][i] == 1:
+                bfs(j,i)
+                groupCnt += 1 
+    print(groupCnt)
