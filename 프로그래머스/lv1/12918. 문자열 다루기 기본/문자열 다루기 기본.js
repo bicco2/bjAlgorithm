32a4009e84e49dfd8a7f7217bfb61601d665d408
bicco2 <77577434+bicco2@users.noreply.github.com>
2023-01-19 17:05:37 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,7 +1,9 @@
 function solution(s) {
-    if(s.length === 4 || s.length === 6){
-        return /^[0-9]+$/.test(s);
-    }
-    return false;
+    // var regex = /^\d{6}$|^\d{4}$/;
+    // if(s.length === 4 || s.length === 6){
+    //     return /^[0-9]+$/.test(s);
+    // }
+    // return false;
+         return /^\d{4}$|^\d{6}$/.test(s);
 
 }
