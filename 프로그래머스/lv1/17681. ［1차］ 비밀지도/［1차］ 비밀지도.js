eb04d83662902974d1de4b8e474a1ef14a1ef13e
bicco2 <77577434+bicco2@users.noreply.github.com>
2023-01-22 22:13:46 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -0,0 +1,25 @@
+function solution(n, arr1, arr2) {
+    var answer = [];
+    var newAns = [];
+    
+    for(let i = 0; i < arr1.length; i++){
+        var arr1Str = arr1[i].toString(2).padStart(n, "0");
+        var arr2Str = arr2[i].toString(2).padStart(n, "0");
+        
+        arr1Str = [...arr1Str];
+        
+        arr1Str = arr1Str.map((item, index) =>
+            (arr1Str[index] === "0" && arr2Str[index] === "0") ? " " : "#"
+        )
+        
+
+        answer.push([...arr1Str]);
+    }
+    
+    
+    for(let i = 0; i < n; i++){
+        newAns.push(answer[i].join(""));
+    }
+
+    return newAns;
+}
