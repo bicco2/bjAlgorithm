eb0cc865c9e9f01ae703a56976394da9a559b160
bicco2 <77577434+bicco2@users.noreply.github.com>
2023-01-29 21:46:24 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -0,0 +1,29 @@
+function solution(n) {
+    var answer = 0;
+    var count = 0;
+    var n_to_binary = [...n.toString(2)];
+    n_to_binary.forEach((item)=> {
+        if(item === "1"){
+            count++;
+        }
+    });
+    
+    while(true){
+        var next_count = 0;
+        n++;
+        var next_binary = [...n.toString(2)];
+        next_binary.forEach((item)=> {
+            if(item === "1"){
+                next_count++;
+            }
+        });
+        if(next_count === count){
+            return n;
+        }
+    }
+    
+    console.log(count);
+
+    
+    return answer;
+}
