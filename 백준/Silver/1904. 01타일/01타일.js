11d92eae6a69e1db0c2fc8b533a2f89c6afb34b2
bicco2 <77577434+bicco2@users.noreply.github.com>
2023-02-10 01:51:06 +0900

    [Silver III] Title: 01타일, Time: 208 ms, Memory: 25448 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim();
+
+let dp = [...new Array(+input + 1)].fill(0);
+
+dp[1] = 1;
+dp[2] = 2;
+
+for (let i = 3; i <= +input; i++) {
+  dp[i] = (dp[i - 2] + dp[i - 1]) % 15746;
+}
+
+console.log(dp[input]);
