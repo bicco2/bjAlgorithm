760281fa02d4dfb2050d37b59c08162b3ea4e33c
bicco2 <77577434+bicco2@users.noreply.github.com>
2023-02-10 22:46:55 +0900

    [Gold V] Title: 합분해, Time: 188 ms, Memory: 9928 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.js
+++ 백준/Gold/2225. 합분해/합분해.js
@@ -1,25 +1,17 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-const [n, k] = fs
+const [N, K] = fs
   .readFileSync(filePath)
   .toString()
   .trim()
   .split(" ")
   .map(Number);
 
-const dp = [...new Array(n + 1)].map((v) => new Array(k + 1).fill(0));
-const MOD = 1000000000;
-
-for (let i = 0; i <= k; i++) {
-  dp[1][i] = i;
-}
-for (let i = 0; i <= n; i++) {
-  dp[i][1] = 1;
-}
-for (let i = 2; i <= n; i++) {
-  for (let j = 2; j <= k; j++) {
-    dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % MOD;
+const dp = [...new Array(K + 1)].map((_) => new Array(N + 1).fill(1));
+for (let i = 2; i <= K; i++) {
+  for (let j = 1; j <= N; j++) {
+    dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % 1000000000;
   }
 }
 
-console.log(dp[n][k]);
+console.log(dp[K][N]);
