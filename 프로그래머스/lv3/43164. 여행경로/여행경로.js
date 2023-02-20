8acc5badf3871c4468375e4c8ad74e2d107e0dc8
bicco2 <77577434+bicco2@users.noreply.github.com>
2023-02-21 00:27:13 +0900

    [level 3] Title: 여행경로, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv3/43164. 여행경로/여행경로.js
+++ 프로그래머스/lv3/43164. 여행경로/여행경로.js
@@ -1,22 +1,22 @@
 function solution(tickets) {
+    tickets.sort(); // 글자순 정렬
+    let vis=Array(tickets.length).fill(false);
     var answer = [];
-    var vis = [...new Array(tickets.length)].map(v=>false);
-
-    tickets.sort();
-    
     function dfs(cur,cnt,path){
-        if(cnt===tickets.length && answer.length===0){ //정렬했으므로 처음오는 경우의 수가 답
+        if(cnt===tickets.length){ //정렬했으므로 처음오는 경우의 수가 답
             answer=path;
-            return;
+            return true;
         }
         for(let i=0;i<tickets.length;i+=1){
             if(vis[i])continue;
             if(tickets[i][0]===cur){ // 출발하는 공항이 같다.
                 vis[i]=true;
-                dfs(tickets[i][1],cnt+1,[...path,tickets[i][1]]);//배열 복사해서 넣어주기
+                if (dfs(tickets[i][1],cnt+1,[...path,tickets[i][1]])){return true;}
                 vis[i]=false;
             }
         }
+        
+        return false;
     }
     dfs("ICN",0,["ICN"])
     return answer;
