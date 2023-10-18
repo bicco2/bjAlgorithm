1c9f7ca5237488299ca83474c8ec3a355d98c2d5
bicco2 <77577434+bicco2@users.noreply.github.com>
2023-10-19 00:47:32 +0900

    [level unrated] Title: rny_string, Time: 0.12 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181863. rny＿string/rny＿string.js
@@ -0,0 +1,12 @@
+function solution(rny_string) {
+    var answer = '';
+    for(let item of rny_string){
+        if(item === "m"){
+            answer += "rn"
+        }
+        else {
+            answer += item;
+        }
+    }
+    return answer;
+}
