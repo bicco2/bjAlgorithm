49182d346cb5609f4dbea86ce47fe7413ab5f67e
bicco2 <77577434+bicco2@users.noreply.github.com>
2024-06-21 21:05:24 +0900

    Time: 51 ms (74.35%), Space: 49.6 MB (32.84%) - LeetHub

---
+++ LeetCode/Medium/0062-unique-paths/0062-unique-paths.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function(m, n) {
+    let board = new Array(m).fill().map(() => new Array(n).fill(1));
+
+    for(let i = 1; i < m; i++){
+        for(let j = 1; j < n; j++){
+            board[i][j] = board[i-1][j] + board[i][j-1];
+        }
+    }
+    
+    return board[m-1][n-1]
+};
